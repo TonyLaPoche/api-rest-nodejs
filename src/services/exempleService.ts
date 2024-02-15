556547c3018ad0611620234c736c823ffcd4f66f
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-15 15:42:53 +0100

    Ajout du service ExempleService pour la création, la recherche, la mise à jour et la suppression d'objets Exemple.

---
+++ src/services/exempleService.ts
@@ -0,0 +1,21 @@
+import { ExempleModel } from '../models/ExempleModel';
+import { ExempleData } from '../types/exempleTypes';
+
+export const ExempleService = {
+  create: async (data: ExempleData) => {
+      const Exemple = new ExempleModel(data);
+      return Exemple.save();
+  },
+  findAll: async () => {
+      return ExempleModel.find();
+  },
+  findById: async (id: string) => {
+      return ExempleModel.findById(id);
+  },
+  update: async (id: string, data: ExempleData) => {
+      return ExempleModel.findByIdAndUpdate(id, data, { new: true });
+  },
+  delete: async (id: string) => {
+      return ExempleModel.deleteOne({ _id: id });
+  },
+};
