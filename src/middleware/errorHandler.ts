725317700d94e580fced40c2f02fc1845e927448
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-15 19:32:46 +0100

    Mise à jour du gestionnaire d'erreurs dans le middleware

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export const errorHandler = (err:Error, req:Request, res:Response, next:NextFunction) => {
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).send({ status: `code ${statusCode}`, type: err.name, message: err.message });
 }
