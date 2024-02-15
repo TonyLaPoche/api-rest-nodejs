9bb7d5016c5496141e8f4d359e5ebb7bf0dd0e48
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-15 15:39:58 +0100

    Ajouter le fichier app.ts avec la configuration du serveur Express et la connexion à la base de données

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import { router as indexRouter } from './routes/index';
+import { connectDB } from './config/db';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+
+(async () => {
+    try {
+      await connectDB();
+      app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+      });
+    } catch (error) {
+      console.error("Database connection failed", error);
+      process.exit(1);
+    }
+  })();
