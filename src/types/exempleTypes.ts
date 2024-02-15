a078e0d24622e5abdb2e83f3e57536048a0ad44a
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-15 15:43:00 +0100

    Ajout des types d'exemple

---
+++ src/types/exempleTypes.ts
@@ -0,0 +1,13 @@
+export interface ExempleData {
+    name: string;
+    description?: string;
+}
+
+export interface UpdateExempleData {
+    name?: string;
+    description?: string;
+}
+
+export interface Exemple extends ExempleData{
+    _id: string;
+}
