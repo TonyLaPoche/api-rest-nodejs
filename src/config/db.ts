787e2c4650e5efd0bdf33dcb5eaf59445b5e36f8
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-15 15:42:17 +0100

    Ajouter la configuration de la base de données

---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+// base de données
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || '');
+    console.log("Database connected");
+  } catch (error) {
+    console.log("Database connection failed", error);
+  }
+};
