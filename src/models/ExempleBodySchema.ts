19092c34e370c17e6b67e0b50a98246993fd3212
tonylapoche <terrade.antoine.pro@gmail.com>
2024-02-20 12:55:03 +0100

    Mise à jour du schéma de création et de mise à jour de l'exemple

--- src/models/ExempleBodySchema.ts
+++ src/models/ExempleBodySchema.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 
-export const ExempleBodySchema = z.object({
-  name: z.string().min(3, {message: 'Valeur trop courte'}).max(30, {message: 'Valeur trop longue'}),
-  description: z.string().max(300 , {message: 'Valeur trop longue'}).nullable().optional(),
-}).required();
+export const ExempleCreationSchema = z.object({
+  name: z.string().min(3).max(30),
+  description: z.string().max(300).nullable().optional(),
+}).strict();
+
+export const ExempleUpdateSchema = z.object({
+  name: z.string().min(3).max(30).optional(),
+  description: z.string().max(300).nullable().optional(),
+}).partial().strict();
